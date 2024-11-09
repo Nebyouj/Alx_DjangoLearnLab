8ad420b423feecb0fda546732058be50a57035d4
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-11-09 15:21:27 +0300

    Master Django’s ORM capabilities by creating a set of models that demonstrate the use of ForeignKey, ManyToMany, and OneToOne relationships.

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2yr$uh@08du2@jysi@fgm3&w9etndj=m))rmyua6zufcu(n2gt'
+SECRET_KEY = 'django-insecure-y#b54k*uu@c=7e7tfn&d3fg@ifj@3hy2uly#@9ia_(kw8+@8g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
