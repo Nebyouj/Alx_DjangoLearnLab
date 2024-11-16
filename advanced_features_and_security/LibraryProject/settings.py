7c220d4351c78ad6df13d87f4660981d939a2b32
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-11-16 07:58:30 +0300

    Customize Django’s user model to suit the specific needs of your application, demonstrating an understanding of extending Django’s authentication system.

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf.apps.BookshelfConfig',
     'relationship_app.apps.RelationshipAppConfig',
 ]
 
@@ -122,4 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
