4dc2c75d57a08d632d53859a9878d45744322e87
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-12-07 04:08:05 +0300

    Enhance your Django blog project by adding tagging and search functionalities. This will allow users to categorize posts via tags and search for posts based on keywords, improving the navigability and user experience of your blog.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -27,3 +28,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content','tags']
+    widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+        }
